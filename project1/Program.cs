65bf180748d504f6b1b705f59ccd58f5ed71811b
franklin nieras <tcc.0182.2020@gmail.com>
2024-06-01 08:55:53 +0800

    first commit

---
+++ project1/Program.cs
@@ -0,0 +1,10 @@
+﻿        string firstName = "John Franklin"; 
+        string middleName = "Perez"; 
+        string lastName = "Nieras"; 
+        int age = 22; 
+        string gender = "Male"; 
+
+        string fullName = $"{firstName} {middleName} {lastName}"; 
+
+        Console.WriteLine($"Hello! My full name is {fullName}.");
+        Console.WriteLine($"I am {age} years old and my gender is {gender}.");
